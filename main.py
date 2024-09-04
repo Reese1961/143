05f7a4d8aa22e2c3ca5600b9b7039eda48df2544
Reese1961 <jordancross2008@gmail.com>
2024-09-04 08:23:10 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+#setup variables
+password = "070808"
+passcode = "031907"
+#input
+
+password = input("Enter password: ")
+passcode = input("Enter passcode: ")
+#processing
+
+if password == "070808":
+  print("Access Granted")
+if passcode == "031907":
+  print("1. Your eyes give a soft gaze when you look my way.")
+  print("2. Your smile warms my heart.")
+  print("3. Your voice calms me like a sweet melody sung by a mother.")
+  print("4. Your presence is a gift to me.")
+  print("5. Your laughter is a melody that lifts my spirits.")
+  print("6. Your touch is like a spark that ignites my senses.")
+  print("8. Your kind heart allows me to be myself.")
+  print("9. Your honest soul is my anchor")
+  print("10. Your hope is my guide")
+  print("11. Your faith in me gives strength")
+  print("12. Your embrace makes me feel loved.")
+  print("13. Your love is a reminder of my worth.")
+  print(" ")
+  print("1. Your beauty is uncomparable to any.")
+  print("2. Your peace is my comfort.")
+  print("3. Your happieness is my source of joy.")
+  print("4. Your kindness is pure and honest.")
+  print("5. Your kiss is soft and sweeter than nectar.")
+  print("6. You make me feel on top of the world.")
+  print("7. You are the queen of my world.")
+  print("8. My heart is yours.")
+  print("9. My love is forever for.")
+  print("10. I want nothing more than to grow old with you.")
+  print("11. You are an image of God's love for me.")
+  print(" ")
+  print("13*11 = 143")
+  print("143 forever My Queen")
+else:
+  print("Access denied")
